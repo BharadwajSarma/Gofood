65268e18b46ce8c9fd84488b7623066769269fe5
BharadwajSarma <chickkubharadwajsarma@gmail.com>
2024-12-23 19:22:54 +0530

    Update Signup.js

--- foodgo/src/screens/Signup.js
+++ foodgo/src/screens/Signup.js
@@ -15,7 +15,7 @@ function Signup() {
         }
 
         try {
-            const response = await axios.post("http://localhost:5000/api/createuser", {
+            const response = await axios.post("https://foodgo-backend-r06p.onrender.com", {
                 name: crede.name,
                 email: crede.email,
                 password: crede.password,
